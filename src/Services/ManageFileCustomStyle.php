ebd16e19e780284282b1ec2dcb75a121ed80f10d
kouwa stephane <kksasteph888@gmail.com>
2023-09-29 07:05:21 +0200

    correct file

--- src/Services/ManageFileCustomStyle.php
+++ src/Services/ManageFileCustomStyle.php
@@ -35,18 +35,29 @@ class ManageFileCustomStyle extends ControllerBase {
   }
   
   /**
+   * --
    *
    * @return string
    */
   protected function getPath() {
     if (!$this->path) {
-      if (!$this->theme_name) {
-        $conf = ConfigDrupal::config('system.theme');
-        $this->theme_name = $conf['default'];
-      }
+      $this->getSelectedTheme();
       $this->path = DRUPAL_ROOT . '/' . $this->ExtensionPathResolver->getPath('theme', $this->theme_name) . '/wbu-atomique-theme/src';
     }
     return $this->path;
+  }
+  
+  /**
+   * --
+   *
+   * @return string
+   */
+  protected function getSelectedTheme() {
+    if (!$this->theme_name) {
+      $conf = ConfigDrupal::config('system.theme');
+      $this->theme_name = $conf['default'];
+    }
+    return $this->theme_name;
   }
   
   /**
@@ -110,8 +121,11 @@ class ManageFileCustomStyle extends ControllerBase {
    * @param string $module
    * @param string $scss
    * @param string $js
+   * @param array $customValue,
+   *        permet de passer des valeurs specique unqiuement lors de la
+   *        creation.
    */
-  public function saveStyle($key, $module, $scss, $js) {
+  public function saveStyle($key, $module, $scss, $js, $customValue = []) {
     $entity = FilesStyle::loadByName($key, $module);
     if ($entity) {
       $entity->setScss($scss);
@@ -124,7 +138,7 @@ class ManageFileCustomStyle extends ControllerBase {
         'module' => $module,
         'scss' => $scss,
         'js' => $js
-      ];
+      ] + $customValue;
       $entity = FilesStyle::create($values);
       $entity->save();
     }
@@ -146,7 +160,8 @@ class ManageFileCustomStyle extends ControllerBase {
   
   public function generateCustomFile() {
     $entities = FilesStyle::loadMultiple();
-    $scss = '';
+    $variable_file = './' . $this->getSelectedTheme() . '_variables.scss';
+    $scss = '    @use "' . $variable_file . '" as *;    ';
     $js = '';
     foreach ($entities as $entity) {
       $scss .= $entity->getScss();

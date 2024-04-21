cd7f4fd6492e3391e07d94b12dbea6b558fd5a6d
kouwa stephane <kksasteph888@gmail.com>
2024-04-21 04:16:11 +0200

    Maj

--- src/Services/ManageFileCustomStyle.php
+++ src/Services/ManageFileCustomStyle.php
@@ -177,9 +177,9 @@ class ManageFileCustomStyle extends ControllerBase {
     if (!empty($this->getConfigGenerateStyleTheme()['tab1']['vendor_import']['load_custom_in_vendor'])) {
       $scss .= '
 // On charge ces imports afin de pouvoir utiliser @extend.
-@use "@stephane888/wbu-atomique/scss/bootstrap-all.scss";
-@use "@stephane888/wbu-atomique/scss/atome/typography/_default.scss";
-@use "@stephane888/wbu-atomique/scss/molecule/default-class.scss";
+@use "@stephane888/wbu-atomique/scss/bootstrap-all.scss" as *;
+@use "@stephane888/wbu-atomique/scss/atome/typography/_default.scss" as *;
+@use "@stephane888/wbu-atomique/scss/molecule/default-class.scss" as *;
 ';
     }
     $js = '';
